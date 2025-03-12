6915583c0e11db16ca38bc69771c1a8b3e50186a
Durga Singh <139614868+Durga-Singh@users.noreply.github.com>
2025-03-12 08:50:24 +0530

    Update p8.R

--- p8.R
+++ p8.R
@@ -5,3 +5,13 @@ print(kc)
 table(iris$Species, kc$cluster)
 plot(newiris[c("Sepal.Length","Sepal.Width")], col=kc$cluster)
 points(kc$centers[,c("Sepal.Length","Sepal.Width")],col=1:3,pch=8,cex=2)
+
+
+newiris<- iris
+newiris$Species<-NULL
+kc<-kmeans(newiris, 3)
+print(kc)
+png(file="cluster.png")
+plot(newiris[c("Sepal.Length","Sepal.Width")], col=kc$cluster)
+points(kc$centers[, c("Sepal.Length","Sepal.Width")], col=1:3, pch = 0, cex=2)
+dev.off()
